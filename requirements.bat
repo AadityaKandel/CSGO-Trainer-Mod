db4f5db77e4507b088405a6052219f4d5d2453c1
AK HACKS OR TRICKS <71229713+AadityaKandel@users.noreply.github.com>
2022-05-07 17:51:19 +0545

    Create requirements.bat

---
+++ requirements.bat
@@ -0,0 +1,17 @@
+echo off
+cls
+
+echo Installing ReadWriteMemory library...
+echo.
+
+pip install ReadWriteMemory
+
+echo.
+echo Installing Keyboard Library.....
+echo.
+pip install keyboard
+echo.
+echo.
+echo Everything Installed.... OK
+pause
+exit
